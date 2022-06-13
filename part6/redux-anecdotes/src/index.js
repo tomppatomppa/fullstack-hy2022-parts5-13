4c69dcfd95a0c26a769179fd6265bc389e641dc0
tomi <tomiwest@hotmail.com>
2022-06-13 23:30:51 +0300

    6.1: unicafe revisited, step1

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import reducer from './reducer'
 
 const store = createStore(reducer)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD',
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'GOOD',
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={bad}>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
